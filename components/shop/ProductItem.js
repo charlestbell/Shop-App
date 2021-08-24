c4fad23c9562252281703b3405d3148bf4b5a690
C.T. Bell <charlestbell@gmail.com>
2021-08-24 14:54:34 -0400

    Adding Product Page

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,15 +1,50 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image, Button } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Button,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from 'react-native';
+
+import Colors from '../../constants/Colors';
 
 const ProductItem = (props) => {
+  let TouchableCmp = TouchableOpacity;
+
+  if (Platform.OS === 'android' && Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback;
+  }
+
   return (
     <View style={styles.product}>
-      <Image style={styles.image} source={{ uri: props.image }} />
-      <Text style={styles.title}>{props.title}</Text>
-      <Text style={styles.price}>${props.price.toFixed(2)}</Text>
-      <View style={styles.actions}>
-        <Button title='View Details' onPress={props.onViewDetail} />
-        <Button title='Cart' onPress={props.onAddToCart} />
+      <View style={styles.touchable}>
+        <TouchableCmp onPress={props.onViewDetail} useForeground>
+          <View>
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={{ uri: props.image }} />
+            </View>
+            <View style={styles.details}>
+              <Text style={styles.title}>{props.title}</Text>
+              <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+            </View>
+            <View style={styles.actions}>
+              <Button
+                color={Colors.primary}
+                title='View Details'
+                onPress={props.onViewDetail}
+              />
+              <Button
+                color={Colors.primary}
+                title='Cart'
+                onPress={props.onAddToCart}
+              />
+            </View>
+          </View>
+        </TouchableCmp>
       </View>
     </View>
   );
@@ -27,19 +62,36 @@ const styles = StyleSheet.create({
     height: 300,
     margin: 20,
   },
-  image: {
+  imageContainer: {
     width: '100%',
     height: '60%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   },
   title: {
     fontSize: 18,
-    marginVertical: 4,
   },
   price: { fontSize: 14, color: '#888' },
   actions: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    height: '25%',
+    paddingHorizontal: 20,
+  },
+  details: {
+    alignItems: 'center',
+    height: '15%',
+    padding: 10,
+  },
+  touchable: {
+    overflow: 'hidden',
+    borderRadius: 10,
   },
 });
 

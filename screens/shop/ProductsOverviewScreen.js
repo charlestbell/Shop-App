ac81df7fee456ff7d06f186ce3d4266048148246
C.T. Bell <charlestbell@gmail.com>
2021-08-25 13:29:29 -0400

    Adding cart list items

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -33,16 +33,23 @@ const ProductsOverviewScreen = (props) => {
     />
   );
 };
-ProductsOverviewScreen.navigationOptions = {
-  headerTitle: 'All Products',
-  headerRight: () => (
-    <HeaderButtons HeaderButtonComponent={HeaderButton}>
-      <Item
-        title='Cart'
-        iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
-        onPress={() => {}}
-      />
-    </HeaderButtons>
-  ),
+ProductsOverviewScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: 'All Products',
+    headerRight: () => {
+      return (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title='Cart'
+            iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+            onPress={() => {
+              navData.navigation.navigate('Cart');
+            }}
+          />
+        </HeaderButtons>
+      );
+    },
+  };
 };
+
 export default ProductsOverviewScreen;

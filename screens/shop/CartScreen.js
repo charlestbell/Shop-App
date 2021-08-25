105095a3e4f47f5114901e34917b49f8a461d760
C.T. Bell <charlestbell@gmail.com>
2021-08-25 14:07:47 -0400

    Making delete button work

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, StyleSheet, Text, Button, FlatList } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
+import * as cartActions from '../../store/actions/cart';
 
-const CartScreen = (props) => {
+const CartScreen = () => {
   const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
   const cartItems = useSelector((state) => {
     const transformedCartItems = [];
@@ -19,8 +20,12 @@ const CartScreen = (props) => {
       });
     }
 
-    return transformedCartItems;
+    return transformedCartItems.sort((a, b) =>
+      a.productId > b.productId ? 1 : -1
+    );
   });
+
+  const dispatch = useDispatch();
 
   return (
     <View style={styles.screen}>
@@ -43,7 +48,9 @@ const CartScreen = (props) => {
             quantity={itemData.item.quantity}
             title={itemData.item.productTitle}
             amount={itemData.item.sum}
-            onRemove={() => {}}
+            onRemove={() => {
+              dispatch(cartActions.removeFromCart(itemData.item.productId));
+            }}
           />
         )}
       />

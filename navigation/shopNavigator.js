1b7af0f8c54a0cac842575cd633db95dbc41ed70
C.T. Bell <charlestbell@gmail.com>
2021-08-25 16:29:09 -0400

    Fixing merge issue

--- navigation/shopNavigator.js
+++ navigation/shopNavigator.js
@@ -24,19 +24,6 @@ const defaultNavOptions = {
   headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
 };
 
-const defaultNavOptions = {
-  headerStyle: {
-    backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
-  },
-  headerTitleStyle: {
-    fontFamily: 'open-sans-bold',
-  },
-  headerBackTitleStyle: {
-    fontFamily: 'open-sans',
-  },
-  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
-};
-
 const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,

bb7952597e96cee83ff8fd2662ee93b6c0b47532
C.T. Bell <charlestbell@gmail.com>
2021-09-03 12:49:09 -0400

    Adding save orders to database

--- store/actions/products.js
+++ store/actions/products.js
@@ -38,12 +38,16 @@ export const fetchProducts = () => {
 };
 export const deleteProduct = (productId) => {
   return async (dispatch) => {
-    await fetch(
+    const response = await fetch(
       `https://rn-shop-app-88655-default-rtdb.firebaseio.com/products/${productId}.json`,
       {
         method: 'DELETE',
       }
     );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
 
     dispatch({ type: DELETE_PRODUCT, pid: productId });
   };
@@ -84,7 +88,7 @@ export const createProduct = (title, description, imageUrl, price) => {
 
 export const updateProduct = (id, title, description, imageUrl) => {
   return async (dispatch) => {
-    await fetch(
+    const response = await fetch(
       `https://rn-shop-app-88655-default-rtdb.firebaseio.com/products/${id}.json`,
       {
         method: 'PATCH',
@@ -98,6 +102,10 @@ export const updateProduct = (id, title, description, imageUrl) => {
         }),
       }
     );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
 
     dispatch({
       type: UPDATE_PRODUCT,

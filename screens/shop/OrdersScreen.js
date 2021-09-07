58400e5700ba84068e861f9855d4f0ba819ae892
C.T. Bell <charlestbell@gmail.com>
2021-09-07 11:15:27 -0400

    Adding no orders, no products messaging

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -35,6 +35,14 @@ const OrdersScreen = () => {
     );
   }
 
+  if (orders.length === 0) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>No orders found. Maybe start ordering some products?</Text>
+      </View>
+    );
+  }
+
   return (
     <FlatList
       data={orders}

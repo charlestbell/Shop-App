2a851aeb442d98d92bb009af46511dbf5f4b2988
C.T. Bell <charlestbell@gmail.com>
2021-09-07 12:52:28 -0400

    Adding auto login

--- navigation/shopNavigator.js
+++ navigation/shopNavigator.js
@@ -12,6 +12,7 @@ import OrdersScreen from '../screens/shop/OrdersScreen';
 import UserProductsScreen from '../screens/user/UserProductsScreen';
 import EditProductScreen from '../screens/user/EditProductsScreen';
 import AuthScreen from '../screens/user/AuthScreen';
+import StartupScreen from '../screens/StartupScreen';
 import Colors from '../constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -108,6 +109,7 @@ const AuthNavigator = createStackNavigator(
 );
 
 const MainNavigator = createSwitchNavigator({
+  Startup: StartupScreen,
   Auth: AuthNavigator,
   Shop: ShopNavigator,
 });
